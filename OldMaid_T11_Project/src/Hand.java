a73deea7955f8f8d26529c66f48c5b1ecb2bd1e5
atravisfields <47760837+atravisfields@users.noreply.github.com>
2019-04-09 16:58:17 -0500

    Updated Player and Hand classes to have their methods
    Inside Player, chooseCard() needs the graphics class to function, so it by default takes the first card from the target player.

--- OldMaid_T11_Project/src/Hand.java
+++ OldMaid_T11_Project/src/Hand.java
@@ -1,4 +1,50 @@
+import java.util.*;
 
-public class Hand {
-
+class Hand {
+	public Integer handSize;
+	protected List<Card> myHand;
+	
+	public void reorderHand() {
+		Collections.shuffle(myHand);
+	}
+	
+	public List<Card> discardPairs() {
+		List<Card> discard = new ArrayList<Card>();
+		Card firstCard, secondCard;
+		
+		for(int i = 0; i < myHand.size(); ++i) {
+			firstCard = myHand.get(i);
+			for(int j = i + 1; j < myHand.size(); ++j) {
+				secondCard = myHand.get(j);
+				if(firstCard.getValue() == secondCard.getValue()) {
+					myHand.remove(i);
+					myHand.remove(j);
+					discard.add(firstCard);
+					discard.add(secondCard);
+				}
+			}
+		}
+		
+		return discard;
+	}
+	
+	public void addCard(Card newCard) {
+		myHand.add(newCard);
+		this.discardPairs();
+	}
+	
+	public void removeCard(Player targetPlayer, int targetIndex) {
+		Card targetCard = targetPlayer.hand.myHand.get(targetIndex);
+		targetPlayer.takeCard(targetIndex);
+		this.addCard(targetCard);
+	}
+	
+	public Hand() {
+		myHand = new ArrayList<Card>();
+		handSize = 0;
+	}
+	public Hand(List<Card> l) {
+		myHand = l;
+		handSize = l.size();
+	}
 }
--- OldMaid_T11_Project/src/Player.java
+++ OldMaid_T11_Project/src/Player.java
@@ -1,4 +1,22 @@
 
-public class Player {
-
+class Player {
+	public Hand hand;
+	
+	void chooseCard(Player targetPlayer) {
+		// This function requires the graphics class
+		int targetIndex = 0;
+		hand.removeCard(targetPlayer, targetIndex);
+	}
+	
+	void takeCard(int numCard) {
+		this.hand.myHand.remove(numCard);
+	}
+	
+	public Player() {
+		hand = new Hand();
+	}
+	
+	public Player(Hand newHand) {
+		hand = newHand;
+	}
 }
