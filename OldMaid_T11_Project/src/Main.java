832d0c179288219665987ed913ffd26357fe0e3c
dingoose <vihasg32@gmail.com>
2019-04-09 22:35:51 -0500

    Added code for main class that could be done without server knowledge or a completed Graphics class

--- OldMaid_T11_Project/src/Main.java
+++ OldMaid_T11_Project/src/Main.java
@@ -1,9 +1,38 @@
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		Game game = new Game();
+		Graphics graphics = new Graphics();
+		replay(game);
+	}
+	
+	public void play()
+	{
+		
+	}
+	
+	public void create()
+	{
+		
+	}
+	
+	public void join()
+	{
+		
+	}
+	
+	public void setup()
+	{
+		
+	}
+	
+	static protected void replay(Game game)
+	{
+		if (game.gameIsOver)
+			game = new Game();
 	}
 
 }
