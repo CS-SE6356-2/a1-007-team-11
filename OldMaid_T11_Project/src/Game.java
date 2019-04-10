8fb3e4275ce29c2c1d6383385714f5c5bc36c831
dingoose <vihasg32@gmail.com>
2019-04-09 22:34:59 -0500

    Added the code for the game class for anything done today

--- OldMaid_T11_Project/src/Game.java
+++ OldMaid_T11_Project/src/Game.java
@@ -1,4 +1,15 @@
+import java.util.*;
+public class Game 
+{
+	protected int numPlayers;
+	private List<Card> discardPile = new ArrayList<>();
+	private Deck deck = new Deck();
+	private List<Player> playerList = new ArrayList<>();
+	protected boolean gameIsOver = false;
 
-public class Game {
-
+	
+	public void gameOver()
+	{
+		gameIsOver = true;
+	}
 }
